e701a94333af3618bb874bedf0e40d0ae8345c88
DESKTOP-F940KKR\Marius <Marius@nereikia.lt>
2018-01-03 00:09:27 +0200

    AOS action on scroll added to the page. still fixing opacity on floating headline in the main section.

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,28 @@
 $(window).scroll(function() {
 
     var wScroll = $(this).scrollTop();
+    var top = $('nav').scrollTop();
     
     $(".absolute-left-bottom").css({
+        'transform' : 'translate(0px, '+ wScroll /2 +'%)'
+        
+    });
+    $(".btn-container").css({
         'transform' : 'translate(0px, '+ wScroll /2 +'%)'
     });
     
     console.log(wScroll);
     
-
+    
+        // var navToTop = Math.floor((wScroll / 100));
+        // console.log('navTop: ' + navToTop);
+        console.log('navTop: ' + top);
+        
+    
 });
+
+AOS.init({
+    duration: 1200
+   });
+// opacity: 1;
+
