d772612eeec6c9743513d39efab7f57f1956c564
DESKTOP-F940KKR\Marius <Marius@nereikia.lt>
2018-01-16 13:28:47 +0200

    font awesome 5 downlloaded and referenced

--- src/index.php
+++ src/index.php
@@ -3,6 +3,11 @@
 <body>
 
 labukas
+<!--user icon in two different styles-->
+<i class="fas fa-user"></i>
+  <i class="far fa-user"></i>
+  <!--brand icon-->
+  <i class="fab fa-github-square"></i>
 
 
 
--- src/load/head.php
+++ src/load/head.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="bootstrap.css">
-    <link rel="stylesheet" href="font-awesome.min.css">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./css/bootstrap.css">
+    <!-- <link rel="stylesheet" href="font-awesome.min.css"> -->
+    <link rel="stylesheet" href="./css/fontawesome-all.css">
+    <link rel="stylesheet" href="./css/style.css">
 
     <title>Sapnų Gaudyglės</title>
 </head>
