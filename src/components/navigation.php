8504636d1a42fe6e9b75a252d0fd8862f97a741e
DESKTOP-F940KKR\Marius <Marius@nereikia.lt>
2018-01-17 11:10:31 +0200

    Front page carousel healine and images are complete some more css needed

---
+++ src/components/navigation.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Sapnų Gaudyklės</a>
+        <!-- Toggler -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHide" aria-controls="navbarHide" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <!-- What is toggled -->
+        <div class="collapse navbar-collapse justify-content-end " id="navbarHide">
+            <div class="navbar-nav mx-2  ">
+                <a class="nav-item mx-2 nav-link active" href="./Pagrindinis.php">  <h5>Pagrindinis</h5>    <span class="sr-only">(current)</span></a>
+                <a class="nav-item mx-2 nav-link" href="./Parduotuve.php">          <h5>Parduotuvė </h5> </a>
+                <a class="nav-item mx-2 nav-link" href="./Apie.php">                <h5>Apie Mane  </h5> </a>
+                <a class="nav-item mx-2 nav-link" href="./Kontaktai.php">           <h5>Kontaktai  </h5> </a>
+            </div>
+        </div>
+    </nav>
--- src/index.php
+++ src/index.php
@@ -2,17 +2,67 @@
 
 <body>
 
-labukas
-<!--user icon in two different styles-->
-<i class="fas fa-user"></i>
-  <i class="far fa-user"></i>
-  <!--brand icon-->
-  <i class="fab fa-github-square"></i>
+    <section class="hero-img-carousel-section container mt-2">
+        <?php include('./components/carousel.php'); ?>    
+    </section>
+
+    <section class="navigation-section">
+        <?php include('./components/navigation.php'); ?>    
+    </section>
+
+    <section class="container">
+        <div class="row">
+            <div class="col-lg-8 mx-auto my-5">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Sapnų Gaudyklė</h5>
+                        <p class="card-text">Sapnų gaudyklė – ne tik jaukus ir originalus miegamojo interjero akcentas. Legendos skelbia, kad šis amuletas apsaugo nuo blogų, piktų minčių ir dvasių, nemalonių sapnų, padeda greičiau užmigti ir miegančiajam siunčia ramius, spalvotus ir džiugius linkėjimus iš sapnų pasaulio, o kai kurie žmonės pasikabina ją savo automobiliuose kaip laimę nešantį ženklą.</p>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </section>
+
+    
+    <section class="container-fluid images-row">
+        <div class="row">
+            <div class="col-lg-2 col-md-4 col-sm-6">
+                <a href="#"><img class="img-thumbnail" src="./img/crop/1.jpg" alt="sapnų gaudyklės nuotrauka"></a>
+            </div>
+            <div class="col-lg-2 col-md-4 col-sm-6">
+                <a href="#"><img class="img-thumbnail" src="./img/crop/2.jpg" alt="sapnų gaudyklės nuotrauka"></a>
+            </div>
+            <div class="col-lg-2 col-md-4 col-sm-6">
+                <a href="#"><img class="img-thumbnail" src="./img/crop/3.jpg" alt="sapnų gaudyklės nuotrauka"></a>
+            </div>
+            <div class="col-lg-2 col-md-4 col-sm-6">
+                <a href="#"><img class="img-thumbnail" src="./img/crop/4.jpg" alt="sapnų gaudyklės nuotrauka"></a>
+            </div>
+            <div class="col-lg-2 col-md-4 col-sm-6">
+                <a href="#"><img class="img-thumbnail" src="./img/crop/5.jpg" alt="sapnų gaudyklės nuotrauka"></a>
+            </div>
+            <div class="col-lg-2 col-md-4 col-sm-6">
+                <a href="#"><img class="img-thumbnail" src="./img/crop/6.jpg" alt="sapnų gaudyklės nuotrauka"></a>
+            </div>
+        </div>
+    </section>
 
 
 
-    <script src="jquery.min.js"></script>
-    <script src="bootstrap.min.js"></script>
-    <script src="script.js"></script>
+
+
+
+
+    <script src="./js/jquery.min.js"></script>
+    <script src="./js/popper.min.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+    
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <!-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script> -->
+  
+    <script src="./js/script.js"></script>
 </body>
 </html>
