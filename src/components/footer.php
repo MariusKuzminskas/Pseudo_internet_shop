e0a31f307b784ae2e6323c47af42bfc96d78c93f
DESKTOP-F940KKR\Marius <Marius@nereikia.lt>
2018-01-18 14:52:50 +0200

    Main page design complete. footer includes in php

---
+++ src/components/footer.php
@@ -0,0 +1,36 @@
+<footer class="container" >
+        <section class="row pt-3 border ">
+            <div class="col-lg-3 pl-5 d-flex flex-column justify-content-center">
+                <h6 class="text-center"> &copy 2018 visos teisės saugomos</h6>
+                <!-- <div><a href="#"> <img class="d-block " src="./img/logo-150.png" alt="sapnų gaudyklės Logotipas"> </a></div>  -->
+            </div>
+            <div class="col-lg-3">
+                <a href="http://www.facebook.com/gaudykles" target="_blank">
+                <i class="fab fa-facebook-square h1 pr-3"></i>
+                </a>
+                <a href="http://www.instagram.com/sapnu.gaudykles">
+                <i class="fab fa-instagram h1 pr-3"></i>
+                </a>
+                <a href="#">
+                <i class="fa fa-envelope h1 pr-3"></i>
+                </a>
+            </div>
+            <div class="col-md-6 mb-2">
+                <h5>instagram</h5>
+                <div class=" " id="instafeed"></div>
+            </div>
+            <div class="col-lg-12 mb-2">
+                
+                <div class=" " id="instafeed"></div>
+            </div>
+        </section>
+</footer>
+
+<script src="./js/jquery.min.js"></script>
+<script src="./js/popper.min.js"></script>
+<script src="./js/bootstrap.min.js"></script>
+
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<!-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script> -->
--- src/index.php
+++ src/index.php
@@ -1,32 +1,25 @@
 <?php include('./load/head.php'); ?>
 
 <body class="bg-light" >
+    <!-- top logo -->
     <div class=""> <img class="d-block mx-auto" src="./img/logo-150.png" alt="sapnų gaudyklės Logotipas"> </div>
-    <section class="hero-img-carousel-section container mt-2">
-        <?php include('./components/carousel.php'); ?>    
-    </section>
-
-    <section class="navigation-section">
+    
+    <section class="container navigation-section">
         <?php include('./components/navigation.php'); ?>    
     </section>
-    <!-- CARD -->
-    <section class="container">
-        <div class="row">
-            <div class="col-lg-8 mx-auto my-5">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Sapnų Gaudyklė</h5>
-                        <p class="card-text">Sapnų gaudyklė – ne tik jaukus ir originalus miegamojo interjero akcentas. Legendos skelbia, kad šis amuletas apsaugo nuo blogų, piktų minčių ir dvasių, nemalonių sapnų, padeda greičiau užmigti ir miegančiajam siunčia ramius, spalvotus ir džiugius linkėjimus iš sapnų pasaulio, o kai kurie žmonės pasikabina ją savo automobiliuose kaip laimę nešantį ženklą.</p>
-                    </div>
-                </div>
-                
+    
+    <section class="hero-img-carousel-section container mt-2">
+    <div class="row py-5">
+            <div class="col-lg-8 mx-auto">
+                <?php include('./components/carousel.php'); ?>    
             </div>
         </div>
     </section>
-
+    
+    
     <!-- THUMBS -->
-    <section class="container-fluid pt-4 border border-left-0 border-right-0 images-row">
-        <div class="row mb-5 border-bottom-1">
+    <section class="container border border-left-0 border-right-0 images-row">
+        <div class="row py-5 border-bottom-1">
             <div class="col-lg-2 col-md-4 col-sm-6">
                 <a href="#"><img class="img-thumbnail" src="./img/crop/1.jpg" alt="sapnų gaudyklės nuotrauka"></a>
             </div>
@@ -47,53 +40,30 @@
             </div>
         </div>
     </section>
-
-    <div class="height-20v"></div>
-
-
-    <?php ?>
-
-    <footer class="container-fluid" >
-        <section class="row pt-3 border ">
-            <div class="col-lg-3 pl-5 d-flex flex-column justify-content-center">
-                <h6 class="text-center"> &copy 2018 visos teisės saugomos</h6>
-                <!-- <div><a href="#"> <img class="d-block " src="./img/logo-150.png" alt="sapnų gaudyklės Logotipas"> </a></div>  -->
-            </div>
-            <div class="col-lg-3">
-                <a href="http://www.facebook.com/gaudykles" target="_blank">
-                <i class="fab fa-facebook-square h1 pr-3"></i>
-                </a>
-                <a href="http://www.instagram.com/sapnu.gaudykles">
-                <i class="fab fa-instagram h1 pr-3"></i>
-                </a>
-                <a href="#">
-                <i class="fa fa-envelope h1 pr-3"></i>
-                </a>
-            </div>
-            <div class="col-md-6 mb-2">
-                <h5>instagram</h5>
-                <div class=" " id="instafeed"></div>
-            </div>
-            <div class="col-lg-12 mb-2">
-                
-                <div class=" " id="instafeed"></div>
-            </div>
-        </section>
-    </footer>
-
-
-
-
-
-
-    <script src="./js/jquery.min.js"></script>
-    <script src="./js/popper.min.js"></script>
-    <script src="./js/bootstrap.min.js"></script>
     
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <!-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script> -->
+    <div class="height-20v"></div>
+    <!-- CARD -->
+    <section class="container">
+        <div class="row">
+            <div class="col-lg-8 mx-auto my-5">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Sapnų Gaudyklė</h5>
+                        <div class="div-row py-1 mx-auto"> </div>
+                        <p class="card-text text-justify ">Sapnų gaudyklė – ne tik jaukus ir originalus miegamojo interjero akcentas. Legendos skelbia, kad šis amuletas apsaugo nuo blogų, piktų minčių ir dvasių, nemalonių sapnų, padeda greičiau užmigti ir miegančiajam siunčia ramius, spalvotus ir džiugius linkėjimus iš sapnų pasaulio, o kai kurie žmonės pasikabina ją savo automobiliuose kaip laimę nešantį ženklą.</p>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </section>
+    
+    
+    <!-- Footer and scripts -->
+    <?php include('./components/footer.php'); ?>    
+
+    
+    
   
     <script src="./js/script.js"></script>
 </body>
