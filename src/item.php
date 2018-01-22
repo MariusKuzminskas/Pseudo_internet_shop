9c72d6a3d250e8fce4d4fda3712c623f76d019cd
DESKTOP-F940KKR\Marius <Marius@nereikia.lt>
2018-01-22 16:12:49 +0200

    db made. db connection ok. populating shop.php with items from db grid problem. need a middle step

---
+++ src/item.php
@@ -0,0 +1,34 @@
+<?php include('./load/head.php'); ?>
+
+<body class="bg-light" >
+    <!-- top logo -->
+    <?php include('./components/logo.php'); ?>
+    <!-- NAV -->
+    <section class="container navigation-section">
+        <?php include('./components/navigation.php'); ?>    
+    </section>
+    
+    <!-- Main -->
+    <section class="container item-container">
+            
+    
+    
+        <?php include('./components/item.php'); ?>    
+    
+
+    </section>
+    
+    
+    
+    
+    
+    
+    <!-- Footer and scripts -->
+    <?php include('./components/footer.php'); ?>    
+
+    
+    
+  
+    <script src="./js/script.js"></script>
+</body>
+</html>
--- src/shop.php
+++ src/shop.php
@@ -12,26 +12,44 @@
     <section class="container shop-container">
         <h4 class="text-center py-5" >Įsigykite sapnų gaudyklę</h4>
     
+
+
         <!-- Item row -->
         <div class="row">
-            <div class="col-lg-4">
+        <div class="col-lg-4">
+        <?php require('./php/dbConnection.php'); 
+        //create query
+        $query = 'SELECT * FROM prekes';
+
+        //Get result in a msqla obj
+        $result = mysqli_query($conn, $query);
+        
+        //fetch row by row
+        while ($preke = mysqli_fetch_assoc($result)) {
+             ?>
+
+
+        
+            
                 <article class="item-container ">
                     <!-- img container -->
                     <div class="img-container position-relative">
-                        <img src="./img/port-thumb/9.jpg" alt="sapnu gaudyklės nuotrauka">
+                        <a href="item.php"><img src="./img/port-thumb/<?php echo $preke['image']?>.jpg" alt="sapnu gaudyklės nuotrauka"></a>
                         <p class="sale bg-dark text-light blockquote text-uppercase px-1 position-absolute"> išpardavimas</p>
                     </div>
                     <!-- under img container -->
-                    <div class="under-img-container text-center">
-                        <h4>pavadinimas</h4>
-                        <p>20 eur</p>
+                    <div class="under-img-container text-center">     
+                        <p><?php echo $preke['pavadinimas']?></p>
+                        <p><s class="text-muted">25 eur</s> <?php echo $preke['kaina']?> eur</p>
                         <a href="#" class="btn btn-outline-dark" >Pirkti</a>
                     </div>
                 </article>
             </div>
         </div>
         
-
+        <?php  } //end while  ?>         
+        
+        
         <!-- item row end -->
         
        
